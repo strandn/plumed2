2fe644a6bac65276daf06e970f8631f4d4d34cec
Nils Strand <nstrand@uchicago.edu>
2024-09-08 13:11:11 -0500

    temporarily disable omp

--- src/ttsketch/TTCross.cpp
+++ src/ttsketch/TTCross.cpp
@@ -163,7 +163,7 @@ void TTCross::updateVb(const vector<vector<double>>& samples) {
   }
   *this->log_ << "Computing Galerkin projection...\n";
   this->log_->flush();
-  unsigned nt = OpenMP::getNumThreads();
+  // unsigned nt = OpenMP::getNumThreads();
   gsl_set_error_handler_off();
   gsl_integration_workspace* workspace = gsl_integration_workspace_alloc(this->aca_n_);
   double result, error;
@@ -176,7 +176,7 @@ void TTCross::updateVb(const vector<vector<double>>& samples) {
 
     if(ii == 1) {
       psi.ref(1) = ITensor(s, prime(l[0]));
-      #pragma omp parallel for num_threads(nt)
+      // #pragma omp parallel for num_threads(nt)
       for(int ss = 1; ss <= dim(s); ++ss) {
         for(int lr = 1; lr <= dim(l[0]); ++lr) {
           // cout << ii << " " << ss << " " << lr << endl;
@@ -208,7 +208,7 @@ void TTCross::updateVb(const vector<vector<double>>& samples) {
       }
     } else if(ii == this->d_) {
       psi.ref(this->d_) = ITensor(s, l[this->d_ - 2]);
-      #pragma omp parallel for num_threads(nt)
+      // #pragma omp parallel for num_threads(nt)
       for(int ss = 1; ss <= dim(s); ++ss) {
         for(int ll = 1; ll <= dim(l[this->d_ - 2]); ++ll) {
           // cout << ii << " " << ss << " " << ll << endl;
@@ -225,7 +225,7 @@ void TTCross::updateVb(const vector<vector<double>>& samples) {
       }
     } else {
       psi.ref(ii) = ITensor(s, l[ii - 2], prime(l[ii - 1]));
-      #pragma omp parallel for num_threads(nt)
+      // #pragma omp parallel for num_threads(nt)
       for(int ss = 1; ss <= dim(s); ++ss) {
         for(int ll = 1; ll <= dim(l[ii - 2]); ++ll) {
           for(int lr = 1; lr <= dim(l[ii - 1]); ++lr) {
