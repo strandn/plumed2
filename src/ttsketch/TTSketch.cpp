3f923073d6945e4229ac4677de1b684b642b8c95
Nils Strand <nstrand@uchicago.edu>
2024-09-11 13:17:08 -0500

    comment out vbias printing

--- src/ttsketch/TTSketch.cpp
+++ src/ttsketch/TTSketch.cpp
@@ -366,20 +366,20 @@ void TTSketch::update() {
     log << "\n";
     log.flush();
     
-    ofstream file;
-    if(this->count_ == 2) {
-      file.open("F.txt");
-    } else {
-      file.open("F.txt", ios_base::app);
-    }
-    for(int i = 0; i < 100; ++i) {
-      double x = -M_PI + 2 * i * M_PI / 100;
-      for(int j = 0; j < 100; ++j) {
-        double y = -M_PI + 2 * j * M_PI / 100;
-        file << x << " " << y << " " << max(ttEval(this->aca_.vb(), this->basis_, { x, y }, false), 0.0) << endl;
-      }
-    }
-    file.close();
+    // ofstream file;
+    // if(this->count_ == 2) {
+    //   file.open("F.txt");
+    // } else {
+    //   file.open("F.txt", ios_base::app);
+    // }
+    // for(int i = 0; i < 100; ++i) {
+    //   double x = -M_PI + 2 * i * M_PI / 100;
+    //   for(int j = 0; j < 100; ++j) {
+    //     double y = -M_PI + 2 * j * M_PI / 100;
+    //     file << x << " " << y << " " << max(ttEval(this->aca_.vb(), this->basis_, { x, y }, false), 0.0) << endl;
+    //   }
+    // }
+    // file.close();
   }
   if(getStep() % this->pace_ == 1) {
     log << "Vbias update " << this->count_ << "...\n\n";
