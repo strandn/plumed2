df4b8fbbbdeec3460a67cb9e26dbe512a0dd89cf
Nils Strand <nstrand@uchicago.edu>
2024-11-22 10:27:00 -0600

    shift f from -2kT to 0

--- src/ttsketch/TTCross.cpp
+++ src/ttsketch/TTCross.cpp
@@ -40,7 +40,7 @@ double TTCross::f(const vector<double>& x) const {
     //TODO: shift to -2kT instead of 0?
     result = max(max(ttEval(this->vb_, this->basis_, x, this->conv_), 0.0) +
                  this->kbt_ * log(max(ttEval(*this->G_, this->basis_, x,
-                 this->convg_), 1.0)) - this->vshift_, -2 * this->kbt_);
+                 this->convg_), 1.0)) - this->vshift_, 0.0);
   }
   return result;
 }
