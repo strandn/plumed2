3e8a9bfb68ac4a938db5a6b21d99f325a1a1372e
Nils Strand <nstrand@uchicago.edu>
2025-01-08 14:23:25 -0600

    some more output

--- src/ttsketch/TTSketch.cpp
+++ src/ttsketch/TTSketch.cpp
@@ -1148,6 +1148,9 @@ double TTSketch::getBias(const vector<double>& cv) {
     for(unsigned i = 0; i < this->count_ - 1; ++i) {
       log << i << "\n";
       log.flush();
+      if(this->mpi_rank_ == 0) {
+        PrintData(this->ttList_[i]);
+      }
       bias += this->kbt_ * std::log(max(ttEval(this->ttList_[i], this->basis_, cv, this->conv_), 1.0)) - this->vshiftList_[i];
       if(bias > 0) {
         this->ttIdxList_.push_back(i);
