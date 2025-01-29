7ef0b9f057e555e182491b43a484ea052e653f49
Nils Strand <nstrand@uchicago.edu>
2025-01-29 15:11:25 -0600

    undo some small changes

--- src/ttsketch/TTSketch.cpp
+++ src/ttsketch/TTSketch.cpp
@@ -257,9 +257,9 @@ TTSketch::TTSketch(const ActionOptions& ao):
   if(this->do_aca_ && (aca_cutoff < 0.0 || aca_cutoff >= 1.0)) {
     error("TTCross requires ACA_CUTOFF that is nonnegative and less than 1");
   }
-  if(aca_cutoff == 0.0) {
-    aca_cutoff = 1.0e-9;
-  }
+  // if(aca_cutoff == 0.0) {
+  //   aca_cutoff = 1.0e-9;
+  // }
   if(this->do_aca_) {
     this->aca_ = TTCross(this->basis_, getkBT(), aca_cutoff, aca_rank, log, !aca_noconv, !noconv, 5 * (nbasis - 1), this->walkers_mpi_);
   }
