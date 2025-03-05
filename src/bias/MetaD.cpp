131027e3857750d7ec4449e6b5f55e9fa28ef185
Nils Strand <nstrand@uchicago.edu>
2025-03-05 11:19:26 -0600

    clean up

--- src/bias/MetaD.cpp
+++ src/bias/MetaD.cpp
@@ -2084,64 +2084,64 @@ void MetaD::update()
     updateFrequencyAdaptiveStride();
   }
 
-  if(getStep() == 1) {
-    std::cout << "rct " << reweight_factor_ << std::endl;
-    std::ofstream file;
-    file.open("phi2phi3_0.txt");
-    for(int i = 0; i < 100; ++i) {
-      double x = -M_PI + 2 * i * M_PI / 100;
-      for(int j = 0; j < 100; ++j) {
-        double y = -M_PI + 2 * j * M_PI / 100;
-        file << x << " " << y << " " << getBias({ x, y, -1.2 }) << std::endl;
-      }
-    }
-    file.close();
-    file.open("phi2phi3_1.txt");
-    for(int i = 0; i < 100; ++i) {
-      double x = -M_PI + 2 * i * M_PI / 100;
-      for(int j = 0; j < 100; ++j) {
-        double y = -M_PI + 2 * j * M_PI / 100;
-        file << x << " " << y << " " << getBias({ x, y, 1.0 }) << std::endl;
-      }
-    }
-    file.close();
-    file.open("phi2phi4_0.txt");
-    for(int i = 0; i < 100; ++i) {
-      double x = -M_PI + 2 * i * M_PI / 100;
-      for(int j = 0; j < 100; ++j) {
-        double y = -M_PI + 2 * j * M_PI / 100;
-        file << x << " " << y << " " << getBias({ x, -1.2, y }) << std::endl;
-      }
-    }
-    file.close();
-    file.open("phi2phi4_1.txt");
-    for(int i = 0; i < 100; ++i) {
-      double x = -M_PI + 2 * i * M_PI / 100;
-      for(int j = 0; j < 100; ++j) {
-        double y = -M_PI + 2 * j * M_PI / 100;
-        file << x << " " << y << " " << getBias({ x, 1.0, y }) << std::endl;
-      }
-    }
-    file.close();
-    file.open("phi3phi4_0.txt");
-    for(int i = 0; i < 100; ++i) {
-      double x = -M_PI + 2 * i * M_PI / 100;
-      for(int j = 0; j < 100; ++j) {
-        double y = -M_PI + 2 * j * M_PI / 100;
-        file << x << " " << y << " " << getBias({ -1.2, x, y }) << std::endl;
-      }
-    }
-    file.close();
-    file.open("phi3phi4_1.txt");
-    for(int i = 0; i < 100; ++i) {
-      double x = -M_PI + 2 * i * M_PI / 100;
-      for(int j = 0; j < 100; ++j) {
-        double y = -M_PI + 2 * j * M_PI / 100;
-        file << x << " " << y << " " << getBias({ 1.0, x, y }) << std::endl;
-      }
-    }
-    file.close();
-  }
+  // if(getStep() == 1) {
+  //   std::cout << "rct " << reweight_factor_ << std::endl;
+  //   std::ofstream file;
+  //   file.open("phi2phi3_0.txt");
+  //   for(int i = 0; i < 100; ++i) {
+  //     double x = -M_PI + 2 * i * M_PI / 100;
+  //     for(int j = 0; j < 100; ++j) {
+  //       double y = -M_PI + 2 * j * M_PI / 100;
+  //       file << x << " " << y << " " << getBias({ x, y, -1.2 }) << std::endl;
+  //     }
+  //   }
+  //   file.close();
+  //   file.open("phi2phi3_1.txt");
+  //   for(int i = 0; i < 100; ++i) {
+  //     double x = -M_PI + 2 * i * M_PI / 100;
+  //     for(int j = 0; j < 100; ++j) {
+  //       double y = -M_PI + 2 * j * M_PI / 100;
+  //       file << x << " " << y << " " << getBias({ x, y, 1.0 }) << std::endl;
+  //     }
+  //   }
+  //   file.close();
+  //   file.open("phi2phi4_0.txt");
+  //   for(int i = 0; i < 100; ++i) {
+  //     double x = -M_PI + 2 * i * M_PI / 100;
+  //     for(int j = 0; j < 100; ++j) {
+  //       double y = -M_PI + 2 * j * M_PI / 100;
+  //       file << x << " " << y << " " << getBias({ x, -1.2, y }) << std::endl;
+  //     }
+  //   }
+  //   file.close();
+  //   file.open("phi2phi4_1.txt");
+  //   for(int i = 0; i < 100; ++i) {
+  //     double x = -M_PI + 2 * i * M_PI / 100;
+  //     for(int j = 0; j < 100; ++j) {
+  //       double y = -M_PI + 2 * j * M_PI / 100;
+  //       file << x << " " << y << " " << getBias({ x, 1.0, y }) << std::endl;
+  //     }
+  //   }
+  //   file.close();
+  //   file.open("phi3phi4_0.txt");
+  //   for(int i = 0; i < 100; ++i) {
+  //     double x = -M_PI + 2 * i * M_PI / 100;
+  //     for(int j = 0; j < 100; ++j) {
+  //       double y = -M_PI + 2 * j * M_PI / 100;
+  //       file << x << " " << y << " " << getBias({ -1.2, x, y }) << std::endl;
+  //     }
+  //   }
+  //   file.close();
+  //   file.open("phi3phi4_1.txt");
+  //   for(int i = 0; i < 100; ++i) {
+  //     double x = -M_PI + 2 * i * M_PI / 100;
+  //     for(int j = 0; j < 100; ++j) {
+  //       double y = -M_PI + 2 * j * M_PI / 100;
+  //       file << x << " " << y << " " << getBias({ 1.0, x, y }) << std::endl;
+  //     }
+  //   }
+  //   file.close();
+  // }
 }
 
 /// takes a pointer to the file and a template std::string with values v and gives back the next center, sigma and height
