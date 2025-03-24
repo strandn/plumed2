f58064774ee83e5cf89f80bb0408a109fcb9d27c
Nils Strand <nstrand@uchicago.edu>
2025-03-24 15:18:12 -0500

    minor

--- src/ttsketch/TTSketch.cpp
+++ src/ttsketch/TTSketch.cpp
@@ -1097,8 +1097,6 @@ pair<vector<ITensor>, IndexSet> TTSketch::intBasisSample(const IndexSet& is) con
   vector<Index> is_new;
   double h = pow(1.0 / N, 1.0 / this->d_);
   for(unsigned i = 1; i <= this->d_; ++i) {
-    double L = (this->basis_[i - 1].dom().second - this->basis_[i - 1].dom().first) / 2;
-    double a = (this->basis_[i - 1].dom().second + this->basis_[i - 1].dom().first) / 2;
     M.push_back(ITensor(sites_new(i), is(i)));
     is_new.push_back(sites_new(i));
     for(unsigned j = 1; j <= N; ++j) {
