27b03bd6bfec26f4e5be41df5d45dac82732ddd3
Nils Strand <nstrand@uchicago.edu>
2025-03-26 14:44:27 -0500

    improved quadrature

--- src/ttsketch/TTSketch.cpp
+++ src/ttsketch/TTSketch.cpp
@@ -212,8 +212,9 @@ TTSketch::TTSketch(const ActionOptions& ao):
     error("TTCross requires positive ACA_RANK");
   }
   if(this->do_aca_) {
+    int nbins = kernel ? this->basis_[0].dx() / 10 : 5 * (nbasis - 1);
     this->aca_ = TTCross(this->basis_, getkBT(), aca_cutoff, aca_rank, log,
-                         !aca_noconv, !noconv, 5 * (nbasis - 1),
+                         !aca_noconv, !noconv, nbins,
                          this->walkers_mpi_, aca_auto_rank);
   }
 
