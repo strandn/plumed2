db1943b2f9126472bb07433cbfface3f24b7443b
Nils Strand <nils.strand17@gmail.com>
2025-06-12 21:14:16 -0500

    try smaller lambda now

--- src/ttsketch/BasisFunc.cpp
+++ src/ttsketch/BasisFunc.cpp
@@ -48,7 +48,7 @@ BasisFunc::BasisFunc(pair<double, double> dom, int nbasis, double w, bool kernel
         }
         this->gram_(i, j) = this->gram_(j, i) = result;
       }
-      this->gram_(i, i) += 1.0e-4;
+      this->gram_(i, i) += 1.0e-8;
     }
     Invert(this->gram_, this->ginv_);
     // pseudoInvert(this->gram_, this->ginv_);
