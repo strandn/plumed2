3a2a4b75655904c315a1f2eb58f5ad9ec464c1b5
Nils Strand <nils.strand17@gmail.com>
2025-06-16 13:57:24 -0500

    try fourier again

--- src/ttsketch/TTOPES.cpp
+++ src/ttsketch/TTOPES.cpp
@@ -202,7 +202,7 @@ TTOPES::TTOPES(const ActionOptions& ao):
     if(interval_max[i] <= interval_min[i]) {
       error("INTERVAL_MAX parameters need to be greater than respective INTERVAL_MIN parameters");
     }
-    this->sketch_basis_.push_back(BasisFunc(make_pair(interval_min[i], interval_max[i]), nbasis, w[i], true, dx[i]));
+    this->sketch_basis_.push_back(BasisFunc(make_pair(interval_min[i], interval_max[i]), nbasis, w[i], false, dx[i]));
   }
 
   parse("SKETCH_LAMBDA", this->sketch_lambda_);
@@ -496,17 +496,17 @@ void TTOPES::paraSketch() {
     }
   }
 
-  for(unsigned i = 1; i <= this->d_; ++i) {
-    auto s = siteIndex(Bemp, i);
-    ITensor ginv(s, prime(s));
-    for(int j = 1; j <= dim(s); ++j) {
-      for(int l = 1; l <= dim(s); ++l) {
-        ginv.set(s = j, prime(s) = l, this->sketch_basis_[i - 1].ginv()(j - 1, l - 1));
-      }
-    }
-    Bemp.ref(i) *= ginv;
-    Bemp.ref(i).noPrime();
-  }
+  // for(unsigned i = 1; i <= this->d_; ++i) {
+  //   auto s = siteIndex(Bemp, i);
+  //   ITensor ginv(s, prime(s));
+  //   for(int j = 1; j <= dim(s); ++j) {
+  //     for(int l = 1; l <= dim(s); ++l) {
+  //       ginv.set(s = j, prime(s) = l, this->sketch_basis_[i - 1].ginv()(j - 1, l - 1));
+  //     }
+  //   }
+  //   Bemp.ref(i) *= ginv;
+  //   Bemp.ref(i).noPrime();
+  // }
 
   G.ref(1) = Bemp(1) * V[1];
 
